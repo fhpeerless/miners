78ca5631f2a24f8e1ff8edb8d8327a4ac6338867
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-05-18 10:03:48 +0800

    Update install_qubic.sh

--- install_qubic.sh
+++ install_qubic.sh
@@ -5,5 +5,6 @@ cd qubic
 tar -zxvf qli-Client-1.9.7-Linux-x64.tar.gz
 sudo wget --no-check-certificate -O /qubic/appsettings.json https://raw.githubusercontent.com/fhpeerless/miners/main/appsettings.json
 sudo /qubic/qli-Client
+sudo systemctl status qli
 
 # sudo mkdir -p /aanode && sudo wget -N --no-check-certificate -O /aanode/install_apphub.sh https://raw.githubusercontent.com/fhpeerless/msn/main/install_apphub.sh && sudo chmod -R 777 /aanode && sudo bash /aanode/install_apphub.sh
