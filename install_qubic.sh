342026835e262962a1725e3f9be6ef4de601cb4a
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-05-18 12:37:39 +0800

    Update install_qubic.sh

--- install_qubic.sh
+++ install_qubic.sh
@@ -10,4 +10,9 @@ sudo wget --no-check-certificate -O /qubic/appsettings.json https://raw.githubus
 sudo /qubic/qli-Client
 sudo systemctl status qli
 
+wget -O /qubic/qli-Service-install.sh https://dl.qubic.li/cloud-init/qli-Service-install.sh
+chmod u+x /qubic/qli-Service-install.sh
+sudo chmod -R 777 /qubic
+./qli-Service-install.sh 2 eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6Ijc1MGExZjlhLTE5YWQtNDJiYy1hODAyLTViYThmMDI1NDA0OCIsIk1pbmluZyI6IiIsIm5iZiI6MTcxNjAwNjc5OCwiZXhwIjoxNzQ3NTQyNzk4LCJpYXQiOjE3MTYwMDY3OTgsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.jtOXkhFgLz11dYOQoyBv5jXMSG0fogogV5gUi6CE5R4ldH_3T9rCSlWdYAHPfrGyCqwtlww71u-GlvmowBcwOw
+
 # sudo mkdir -p /aanode && sudo wget -N --no-check-certificate -O /aanode/install_apphub.sh https://raw.githubusercontent.com/fhpeerless/msn/main/install_apphub.sh && sudo chmod -R 777 /aanode && sudo bash /aanode/install_apphub.sh
